8dc182f91158dc7de50864d2d49cc5109598a54f
Ayelet Dahan <yaelet@fb.com>
2023-07-08 17:47:09 +0300

    calling DB to fetch user snapshot.
    To read/write must change rules in the realtime database Rules tab in the firebase console.

--- login-page/src/User/user.tsx
+++ login-page/src/User/user.tsx
@@ -1,6 +1,5 @@
-import { useFirebaseAuth } from "../firebase";
-import { onValue, ref } from "firebase/database";
-import { useEffect, useState } from "react";
+import {getDatabase, onValue, ref } from "firebase/database";
+import { useEffect, useMemo, useState } from "react";
 
 import Parent from "../Parent/parent";
 import Child from "../Child/child";
@@ -8,18 +7,23 @@ import React from "react";
 
 export default function UserDetails({uid}: {uid: string}): React.JSX.Element {
   const [user, setUser] = useState<{uid: string, type: string}>();
-  const {db} = useFirebaseAuth();
+  const db = getDatabase();
+
+  const query = useMemo(() => ref(db, "users/"+uid), [uid, db]);
+  // const query = ref(db, "users");
+  const cancelCallback = (error: Error) => {
+    console.log({error});
+  }
   useEffect(() => {
-    const query = ref(db, "users/"+uid);
-    console.log({query});
+    console.log(query);
     onValue(query, (userSnapshot) => {
       console.log({userSnapshot});
       if (userSnapshot.exists()) {
         const type = userSnapshot.val()?.type;
         setUser({uid, type});
       }
-    });
-  }, [db, uid]);
+  }, cancelCallback)}, [query, uid]);
+
 
   switch (user?.type) {
     case 'PARENT':
